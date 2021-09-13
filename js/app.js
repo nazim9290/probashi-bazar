dcdff448ac635007b3835058b0d37dc7c8ad1810
Nazim Uddin <nushourav4@gmail.com>
2021-09-13 14:37:44 +0900

    all ok

--- js/app.js
+++ js/app.js
@@ -19,35 +19,20 @@ const showProducts = (products) => {
     const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
+    div.innerHTML = `
+    <div class="single-product">
       <div>
     <img class="product-image" src=${image}></img>
       </div>
+      <div class="product-info">
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h4>Price: $ ${product.price}</h4>
       <div><small>Rating: ${product.rating.rate} - ${product.rating.count} votes </small></div>
-      
+    
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">Details</button></div>
-      <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
